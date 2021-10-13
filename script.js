079958fde0ea4a01c209a75af7264a96d1bb35b3
Cool-cal3b <number1caleb@gmail.com>
2021-10-13 11:00:12 -0600

    fetched coin names

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById("cryptoSubmit").addEventListener("click", (e)=>{
   e.preventDefault();
   console.log("Test");
+
+  const url = "https://api.coinpaprika.com/v1/coins"
+  fetch(url).then(response => response.json()).then(json => {
+    console.log(json);
+  });
 })
