8fba0b1a4b3d3cb8695cfe11297f9c4b0da6cb17
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2021-10-16 11:01:35 -0600

    limited coin response to top 10 hits. added coin.html as a new page

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 document.getElementById("cryptoSubmit").addEventListener("click", (e)=>{
   e.preventDefault();
-  console.log("Test");
+  //console.log("Test");
 
   const url = "https://api.coinpaprika.com/v1/coins"
   fetch(url).then(response => response.json()).then(json => {
     let matchArray = [];
     let input = document.getElementById("cryptoInput").value;
+    let topTen = 10;
     for (ob of json) {
-      if (ob.name.toLowerCase().includes(input.toLowerCase())) matchArray.push(ob);
+      if (ob.name.toLowerCase().includes(input.toLowerCase()) && topTen > 0)
+      {
+        matchArray.push(ob);
+        topTen--;
+      }
     }
     console.log(matchArray);
   });
